08948cb4d7f75f8e1783f0835a31b9b8cdb5d3db
Gabriel <g.lundmark@gmail.com>
2018-03-15 00:34:08 +0100

    tiny refactoring

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ let currentLocation;
 
 // Get geolocation
 document.getElementById('find-me').addEventListener('click', getLocation);
+
 function getLocation() {
     navigator.geolocation.getCurrentPosition((position) => {
         document.querySelector('#coordinates').textContent = `
@@ -25,31 +26,47 @@ function getForecast(city = 'stockholm') {
             let counter = 1;
 
             // Add city and timestamp to elements
-            document.querySelector('#city').textContent = `Weather forecast for ${json.location.name}, ${json.location.country} on ${getDay()}`;
-            document.querySelector('#updated').textContent = `Last updated on ${getDay()} ${json.current.last_updated}`;
+            document.querySelector('#city')
+                .textContent = `Weather forecast for ${json.location.name}, ${json.location.country} on ${getDay()}`;
+            document.querySelector('#updated')
+                .textContent = `Last updated on ${getDay()} ${json.current.last_updated}`;
 
             // Add specific current weather data
-            document.querySelector(`#day-${counter} .feels-like`).textContent = `Feels like ${Math.round(json.current.feelslike_c)} °C`;
-            document.querySelector(`#day-${counter} .pressure`).textContent = `Pressure ${(json.current.pressure_mb / 1000).toFixed(3)} bar`;
+            document.querySelector(`#day-${counter} .feels-like`)
+                .textContent = `Feels like ${Math.round(json.current.feelslike_c)} °C`;
+            document.querySelector(`#day-${counter} .pressure`)
+                .textContent = `Pressure ${(json.current.pressure_mb / 1000).toFixed(3)} bar`;
 
             // Add weather data to elements
             json.forecast.forecastday.forEach(forecastday => {
                 document.querySelector(`#day-${counter} .icon`)
                     .setAttribute('src', `https:${forecastday.day.condition.icon}`);
-                document.querySelector(`#day-${counter} .date`).textContent = forecastday.date;
-                document.querySelector(`#day-${counter} .temp`).textContent = Math.round(forecastday.day.avgtemp_c) + ' °C';
-                document.querySelector(`#day-${counter} .conditions`).textContent = forecastday.day.condition.text;
-                document.querySelector(`#day-${counter} .diff-temp`).textContent = `Temps ${Math.round(forecastday.day.mintemp_c)} to ${Math.round(forecastday.day.maxtemp_c)} °C`;
-                document.querySelector(`#day-${counter} .humidity`).textContent = `Humidity ${forecastday.day.avghumidity}%`;
-                document.querySelector(`#day-${counter} .rain`).textContent = `Rain ${forecastday.day.totalprecip_mm} mm`;
-                document.querySelector(`#day-${counter} .max-wind`).textContent = `Wind ${(forecastday.day.maxwind_kph / 3.6).toFixed(2)} m/s`;
-                document.querySelector(`#day-${counter} .sunrise`).textContent = forecastday.astro.sunrise;
-                document.querySelector(`#day-${counter} .sunset`).textContent = forecastday.astro.sunset;
+                document.querySelector(`#day-${counter} .date`)
+                    .textContent = forecastday.date;
+                document.querySelector(`#day-${counter} .temp`)
+                    .textContent = Math.round(forecastday.day.avgtemp_c) + ' °C';
+                document.querySelector(`#day-${counter} .conditions`)
+                    .textContent = forecastday.day.condition.text;
+                document.querySelector(`#day-${counter} .diff-temp`)
+                    .textContent = `Temps ${Math.round(forecastday.day.mintemp_c)} to ${Math.round(forecastday.day.maxtemp_c)} °C`;
+                document.querySelector(`#day-${counter} .humidity`)
+                    .textContent = `Humidity ${forecastday.day.avghumidity}%`;
+                document.querySelector(`#day-${counter} .rain`)
+                    .textContent = `Rain ${forecastday.day.totalprecip_mm} mm`;
+                document.querySelector(`#day-${counter} .max-wind`)
+                    .textContent = `Wind ${(forecastday.day.maxwind_kph / 3.6).toFixed(2)} m/s`;
+                document.querySelector(`#day-${counter} .sunrise`)
+                    .textContent = forecastday.astro.sunrise;
+                document.querySelector(`#day-${counter} .sunset`)
+                    .textContent = forecastday.astro.sunset;
                 counter++;
             });
+            // Add location to search field
             document.getElementById('city-input').value = json.location.name;
         }).catch(err => {
-            document.getElementById('city-input').value = 'Spell properly';
+            // Handle errors
+            document.getElementById('city-input').placeholder = 'Spell properly';
+            document.getElementById('city-input').value = '';
             document.querySelector('#city').textContent = 'Y U NO forecast?';
             console.log('error:', err.message);
         });
